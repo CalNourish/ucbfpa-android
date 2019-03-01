46d62df8042cd01a62f7f44c2db8d054ddaa8274
Samantha Lai <samanthalai@Samanthas-MacBook-Pro.local>
2019-03-01 12:27:28 -0800

    cleaned up individual item

--- app/src/main/java/com/example/calnourish/IndividualItemActivity.java
+++ app/src/main/java/com/example/calnourish/IndividualItemActivity.java
@@ -1,8 +1,16 @@
 package com.example.calnourish;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,29 +19,98 @@ import com.squareup.picasso.Picasso;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class IndividualItemActivity extends AppCompatActivity {
+    private Button searchBar;
     private TextView textViewName;
     private TextView textViewCount;
     private ImageView imageView;
     private String itemName;
-    private String itemPoint;
+//    private String itemPoint;
     private String itemCount;
     private String itemImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+        getWindow().setStatusBarColor(Color.WHITE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_individual_item);
+        searchBar = (Button) findViewById(R.id.search_bar2);
         textViewName = (TextView) findViewById(R.id.individualItemName);
         textViewCount = (TextView) findViewById(R.id.individualItemCount);
         imageView = (ImageView) findViewById(R.id.individualItemImage);
+
         Intent intent = getIntent();
         itemName = intent.getStringExtra("item_name");
         itemImage = intent.getStringExtra("item_image");
         itemCount = intent.getStringExtra("item_count");
-
         textViewName.setText(itemName);
-        textViewCount.setText(itemCount);
+        setTextViewCount();
+        setImageView();
 
+        searchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent searchIntent = new Intent(IndividualItemActivity.this, SearchActivity.class);
+                startActivity(searchIntent);
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.info:
+                        Intent infoIntent = new Intent(IndividualItemActivity.this, InfoActivity.class);
+                        startActivity(infoIntent);
+                        break;
+
+                    case R.id.category:
+                        Intent categoryIntent = new Intent(IndividualItemActivity.this, MainActivity.class);
+                        startActivity(categoryIntent);
+                        break;
+
+                    case R.id.search:
+                        Intent searchIntent = new Intent(IndividualItemActivity.this, SearchActivity.class);
+                        startActivity(searchIntent);
+                        break;
+
+                    case R.id.foodrecovery:
+                        Intent foodrecoveryIntent = new Intent(IndividualItemActivity.this, FoodRecoveryActivity.class);
+                        startActivity(foodrecoveryIntent);
+                        break;
+
+                    case R.id.menu:
+                        Intent menuIntent = new Intent(IndividualItemActivity.this, MenuActivity.class);
+                        startActivity(menuIntent);
+                        break;
+                }
+
+
+                return false;
+            }
+        });
+    }
+
+    public void setTextViewCount() {
+        textViewCount.setText(itemCount + " IN STOCK");
+        if (Integer.parseInt(itemCount) <= 0) {
+            textViewCount.setText("NOT IN STOCK");
+            textViewCount.setTextColor(ContextCompat.getColor(this, R.color.calNourishText));
+        } else if (Integer.parseInt(itemCount) < 10) {
+            textViewCount.setTextColor(ContextCompat.getColor(this, R.color.calNourishRed));
+        } else if (Integer.parseInt(itemCount) < 20) {
+            textViewCount.setTextColor(ContextCompat.getColor(this, R.color.calNourishAccent));
+        } else {
+            textViewCount.setTextColor(ContextCompat.getColor(this, R.color.calNourishGreen));
+        }
+    }
+
+    public void setImageView() {
         String placeholderImage = "placeholder" + Integer.toString(ThreadLocalRandom.current().nextInt(1, 5 + 1));
         int placeholderImageId = this.getResources().getIdentifier(placeholderImage, "drawable", this.getPackageName());
         Picasso.get()
@@ -41,5 +118,10 @@ public class IndividualItemActivity extends AppCompatActivity {
                 .error(placeholderImageId)
                 .placeholder(placeholderImageId)
                 .into(imageView);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
     }
 }
